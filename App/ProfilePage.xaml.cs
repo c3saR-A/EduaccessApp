1dbd46e2cd85aaf20db91a0683b74bb93e5b455a
Abel270 <abelhollman23@gmail.com>
2024-10-23 12:14:25 -0600

    Canbio imagen perfil

--- App/ProfilePage.xaml.cs
+++ App/ProfilePage.xaml.cs
@@ -5,5 +5,15 @@ public partial class ProfilePage : ContentPage
     public ProfilePage()
     {
         InitializeComponent();
+        //Hacer invisible la barra de navegación
+        Shell.SetNavBarIsVisible(this, false);
+     
+        
+
     }
+    private void BackMainPage(object sender, EventArgs e)
+    {
+        Navigation.PopAsync(); // Para regresar a la página anterior
+    }
+    
 }
